a3d25f9174c8295deab6d9f896ca867955ac551e
kim <scilo200@gmail.com>
2024-10-28 10:11:45 +0900

    init

---
+++ src/main/java/com/zerobase/traveltribe/TravelTribeApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.traveltribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTribeApplication.class, args);
+	}
+
+}

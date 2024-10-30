78a60dddaead206fbd8eef7094a311dc8a748ce4
kim <scilo200@gmail.com>
2024-10-30 11:54:02 +0900

    test : 공통 response에 맞추어 테스트 수정

--- travel/src/test/java/com/zerobase/travel/controller/RatingControllerTest.java
+++ travel/src/test/java/com/zerobase/travel/controller/RatingControllerTest.java
@@ -1,19 +1,17 @@
 package com.zerobase.travel.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zerobase.travel.dto.request.GiveRatingDto;
 import com.zerobase.travel.service.RatingService;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito.BDDMyOngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -48,7 +46,8 @@ class RatingControllerTest {
         mockMvc.perform(post("/api/v1/posts/{postId}/rating", postId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(giveRatingDto)))
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.result").value("SUCCESS"));
 
         verify(ratingService, times(1)).giveRating(any(), anyLong(), anyLong());
     }
--- travel/src/test/java/com/zerobase/travel/controller/VoteControllerTest.java
+++ travel/src/test/java/com/zerobase/travel/controller/VoteControllerTest.java
@@ -1,15 +1,14 @@
 package com.zerobase.travel.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.mockito.BDDMockito.given;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zerobase.travel.dto.request.VoteRequestDto.VoteVoting;
@@ -18,7 +17,6 @@ import com.zerobase.travel.dto.response.VoteResponseDto.GetVote;
 import com.zerobase.travel.service.VoteService;
 import com.zerobase.travel.type.VotingStatus;
 import java.util.List;
-import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -47,7 +45,8 @@ class VoteControllerTest {
         //when
         //then
         mockMvc.perform(post("/api/v1/posts/{postId}/voting-starts", postId))
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.result").value("SUCCESS"));
 
         verify(voteService, times(1)).createVote(anyLong(), anyLong());
 
@@ -73,8 +72,9 @@ class VoteControllerTest {
         //then
         mockMvc.perform(get("/api/v1/posts/{postId}/voting-starts", postId))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.votingStartsId").value(1L))
-            .andExpect(jsonPath("$.votingStatus").value(VotingStatus.STARTING.toString()))
+            .andExpect(jsonPath("$.result").value("SUCCESS"))
+            .andExpect(jsonPath("$.data.votingStartsId").value(1L))
+            .andExpect(jsonPath("$.data.votingStatus").value(VotingStatus.STARTING.toString()))
             .andReturn();
 
         verify(voteService, times(1)).getVotingStart(anyLong(), anyLong());
@@ -93,13 +93,15 @@ class VoteControllerTest {
         VoteVoting voteVoting = new VoteVoting();
         voteVoting.setApproval(true);
 
-
         //when
         //then
-        mockMvc.perform(post("/api/v1/posts/{postId}/voting-starts/{votingStartsId}/votings", postId, votingStartsId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(voteVoting)))
-            .andExpect(status().isOk());
+        mockMvc.perform(
+                post("/api/v1/posts/{postId}/voting-starts/{votingStartsId}/votings", postId,
+                    votingStartsId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(voteVoting)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.result").value("SUCCESS"));
 
         verify(voteService, times(1)).voteVoting(anyLong(), anyLong(), anyLong(), anyBoolean());
 
@@ -136,16 +138,18 @@ class VoteControllerTest {
 
         //when
         //then
-        mockMvc.perform(get("/api/v1/posts/{postId}/voting-starts/{votingStartsId}/votings", postId, votingStartsId))
+        mockMvc.perform(get("/api/v1/posts/{postId}/voting-starts/{votingStartsId}/votings", postId,
+                votingStartsId))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.votings[0].votingId").value(1L))
-            .andExpect(jsonPath("$.votings[0].votingStartId").value(votingStartsId))
-            .andExpect(jsonPath("$.votings[0].userId").value(1L))
-            .andExpect(jsonPath("$.votings[0].approval").value(true))
-            .andExpect(jsonPath("$.votings[1].votingId").value(2L))
-            .andExpect(jsonPath("$.votings[1].votingStartId").value(votingStartsId))
-            .andExpect(jsonPath("$.votings[1].userId").value(2L))
-            .andExpect(jsonPath("$.votings[1].approval").value(false))
+            .andExpect(jsonPath("$.result").value("SUCCESS"))
+            .andExpect(jsonPath("$.data.votings[0].votingId").value(1L))
+            .andExpect(jsonPath("$.data.votings[0].votingStartId").value(votingStartsId))
+            .andExpect(jsonPath("$.data.votings[0].userId").value(1L))
+            .andExpect(jsonPath("$.data.votings[0].approval").value(true))
+            .andExpect(jsonPath("$.data.votings[1].votingId").value(2L))
+            .andExpect(jsonPath("$.data.votings[1].votingStartId").value(votingStartsId))
+            .andExpect(jsonPath("$.data.votings[1].userId").value(2L))
+            .andExpect(jsonPath("$.data.votings[1].approval").value(false))
             .andReturn();
 
         verify(voteService, times(1)).getVote(anyLong(), anyLong(), anyLong());

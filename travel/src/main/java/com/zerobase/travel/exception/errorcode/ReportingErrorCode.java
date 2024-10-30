deba5476276bb62486c35cda1310b9b54d5a0889
kim <scilo200@gmail.com>
2024-10-30 15:47:16 +0900

    chore : 불필요 에러코드 삭제

--- travel/src/main/java/com/zerobase/travel/exception/errorcode/ReportingErrorCode.java
+++ travel/src/main/java/com/zerobase/travel/exception/errorcode/ReportingErrorCode.java
@@ -7,8 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ReportingErrorCode implements ErrorCode {
 
-    ALREADY_REPORTING("ERROR-REPORTING-00001", "이미 신고하였습니다."),
-    SCORE_OUT_OF_UNIT("ERROR-REPORTING-00002", "평점 단위가 다릅니다.");
+    ALREADY_REPORTING("ERROR-REPORTING-00001", "이미 신고하였습니다.");
 
     private final String errorCode;
     private final String errorMessage;

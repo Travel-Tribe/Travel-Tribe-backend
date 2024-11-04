cc52410c7647732e1933218ded286d1264ab2f76
Byeon-Sanghwa <bsh6226@gmail.com>
2024-11-04 09:54:59 +0900

    feat) Participation Entity 상태에 따른 status 도입

---
+++ travel/src/main/java/com/zerobase/travel/entity/ParticipationStatus.java
@@ -0,0 +1,12 @@
+package com.zerobase.travel.entity;
+
+public enum ParticipationStatus {
+    JOIN,
+    JOIN_CANCEL,
+    JOIN_CANCEL_REFUNDED,
+    JOIN_CANCEL_PENTANLTY,
+    TRAVEL_FINISHED,
+    TRAVEL_FINISHED_RATED,
+    TRAVEL_FINISHED_REFUNDED,
+
+}

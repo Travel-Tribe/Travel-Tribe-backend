856c2d5071b02bbcd5ed0647ce0bce4e64e5a083
Byeon-Sanghwa <bsh6226@gmail.com>
2024-11-04 15:06:27 +0900

    feat) 참여자 조회기능 repo 추가

---
+++ travel/src/main/java/com/zerobase/travel/repository/ParticipationRepository.java
@@ -0,0 +1,13 @@
+package com.zerobase.travel.repository;
+
+import com.zerobase.travel.entity.ParticipationEntity;
+import com.zerobase.travel.type.ParticipationStatus;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParticipationRepository extends JpaRepository<ParticipationEntity,Long> {
+
+    List<ParticipationEntity> findByPostEntityIdAndParticipationStatusIn(Long postId ,List<ParticipationStatus> participationStatuses);
+}

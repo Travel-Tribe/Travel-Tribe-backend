8d7a333385d13860cf29e6e62e57955cf68490e4
Byeon-Sanghwa <bsh6226@gmail.com>
2024-11-04 09:54:21 +0900

    feat) participation 기능 생성을 위한 깡통 post entity, postrepo 생성

---
+++ travel/src/main/java/com/zerobase/travel/entity/PostEntity.java
@@ -0,0 +1,12 @@
+package com.zerobase.travel.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class PostEntity {
+
+    @Id
+    private Long postId;
+
+}

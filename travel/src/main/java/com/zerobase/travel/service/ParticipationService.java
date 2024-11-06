f4d7ea169b5585505043de96d3cd57fc7e6d1130
Byeon-Sanghwa <bsh6226@gmail.com>
2024-11-06 15:57:57 +0900

    feat) service 여행참여 횟수 확인기능 추가

--- travel/src/main/java/com/zerobase/travel/service/ParticipationService.java
+++ travel/src/main/java/com/zerobase/travel/service/ParticipationService.java
@@ -1,11 +1,12 @@
 package com.zerobase.travel.service;
 
+import com.zerobase.travel.communities.type.CustomException;
+import com.zerobase.travel.communities.type.ErrorCode;
 import com.zerobase.travel.dto.ParticipationDto;
 import com.zerobase.travel.entity.ParticipationEntity;
 import com.zerobase.travel.post.entity.PostEntity;
-import com.zerobase.travel.type.ParticipationStatus;
 import com.zerobase.travel.repository.ParticipationRepository;
-import java.util.ArrayList;
+import com.zerobase.travel.type.ParticipationStatus;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,15 +22,19 @@ public class ParticipationService {
     /*
        1. 인당 최대 두개 참여가능
        2. 최대 개수를 넘겨서는 안될것
+       3. 여러가지 취향에 따른 제한들
      */
-    public void validateApplicant() {
+    public void validateApplicant(String userId) {
+        if(this.countParticipationsJoinByUserId(userId)>=2){
+            throw new CustomException(ErrorCode.PARTICIPATION_LIMIT);
+        }
 
 
     }
 
     public ParticipationDto createParticipation(Long postId, String userId) {
 
-        this.validateApplicant();
+        this.validateApplicant(userId);
 
         ParticipationEntity participationEntity = ParticipationEntity.builder()
             .postEntity(PostEntity.builder().postId(postId).build())
@@ -41,20 +46,26 @@ public class ParticipationService {
             participationRepository.save(participationEntity));
     }
 
-    public List<ParticipationDto> getParticipationsStatusOfJoinAndJoinReady(
+    public List<ParticipationDto> getParticipationsStatusOfJoin(
         Long postId) {
 
-        List<ParticipationStatus> statuses =
-            new ArrayList<>(
-                List.of(ParticipationStatus.JOIN, ParticipationStatus.JOIN_READY));
 
         List<ParticipationEntity> participationEntities
-            = participationRepository.findByPostEntityPostIdAndParticipationStatusIn(
-            postId, statuses);
+            = participationRepository.findByPostEntityPostIdAndParticipationStatus(
+            postId, ParticipationStatus.JOIN);
 
         return participationEntities.stream().map(ParticipationDto::fromEntity)
             .toList();
     }
 
 
+    public int countParticipationsCompletedByUserId(String userId) {
+
+        return participationRepository.countByParticipationStatusAndUserId(ParticipationStatus.TRAVEL_FINISHED,userId);
+    }
+
+    public int countParticipationsJoinByUserId(String userId) {
+
+        return participationRepository.countByParticipationStatusAndUserId(ParticipationStatus.JOIN,userId);
+    }
 }

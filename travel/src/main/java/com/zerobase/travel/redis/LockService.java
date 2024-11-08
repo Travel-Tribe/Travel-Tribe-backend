7282470bc26ac38945c93c6738fa438c65b75e4b
Byeon-Sanghwa <bsh6226@gmail.com>
2024-11-08 17:18:10 +0900

    feat) Lock service도입

--- travel/src/main/java/com/zerobase/travel/redis/LockService.java
+++ travel/src/main/java/com/zerobase/travel/redis/LockService.java
@@ -1,5 +1,7 @@
 package com.zerobase.travel.redis;
 
+import com.zerobase.travel.communities.type.CustomException;
+import com.zerobase.travel.communities.type.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
@@ -12,13 +14,14 @@ import org.springframework.stereotype.Service;
 public class LockService {
     private final RedissonClient redisson;
 
-    public void lock(String postId) {
+    public void lock(Long postId) {
         RLock lock = redisson.getLock(getLockName(postId));
         log.debug("Trying Lock for PostId : {}", postId);
         try{
             boolean isLock = lock.tryLock();
             if(!isLock){
                 log.debug("Lock failed to get for PostId : {}", postId);
+                throw new CustomException(ErrorCode.PARTICIPATION_LOCK);
             }
         }catch (Exception e){
             log.error("Redis Lock Exception", e);
@@ -26,13 +29,13 @@ public class LockService {
 
     }
 
-    public void unlock(String postId) {
+    public void unlock(Long postId) {
         RLock lock = redisson.getLock(getLockName(postId));
         log.debug("Trying Unlock for PostId : {}", postId);
         lock.unlock();
     }
 
-    private static String getLockName(String lockName) {
+    private static String getLockName(Long lockName) {
         return "TravelPartyPostId :" + lockName;
     }
 

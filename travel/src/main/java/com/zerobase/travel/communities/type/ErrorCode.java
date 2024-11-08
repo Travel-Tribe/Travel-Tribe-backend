3e43031f0d59dfff39d0d9c8fe8279067a781434
Byeon-Sanghwa <bsh6226@gmail.com>
2024-11-08 17:19:14 +0900

    feat)  lock 관련 error code추가도입

--- travel/src/main/java/com/zerobase/travel/communities/type/ErrorCode.java
+++ travel/src/main/java/com/zerobase/travel/communities/type/ErrorCode.java
@@ -1,5 +1,5 @@
 package com.zerobase.travel.communities.type;
 
 public enum ErrorCode {
-    PARTICIPATION_NOT_FOUND, COMMUNITY_NON_EXISTENT
+    PARTICIPATION_NOT_FOUND, PARTICIPATION_LOCK, COMMUNITY_NON_EXISTENT
 }

e2d5ade244f6f638e1ac3f3932762134a4f3d72f
Byeon-Sanghwa <bsh6226@gmail.com>
2024-11-08 11:57:18 +0900

    feat) 보증금 반환예상 시점을 추가하기 위한 엔티티 필드추가

--- travel/src/main/java/com/zerobase/travel/entity/ParticipationEntity.java
+++ travel/src/main/java/com/zerobase/travel/entity/ParticipationEntity.java
@@ -11,11 +11,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 
 @Entity
@@ -32,12 +37,6 @@ public class ParticipationEntity {
     @ManyToOne
     private PostEntity postEntity;
     private String userId;
-    @Enumerated(EnumType.STRING)
-    private ParticipationStatus participationStatus;
-    @Enumerated(EnumType.STRING)
-    private DepositStatus depositStatus;
-    @Enumerated(EnumType.STRING)
-    private RatingStatus ratingStatus;
 
     /*
     ParticipationEntity의 상태에 대해서 3가지로 나눈이유
@@ -46,10 +45,30 @@ public class ParticipationEntity {
 
     2. 마케팅 관점에서도 유의미한 데이터단위로 나누어야하기때문에 3가지 상태는 분리되어야생각하였음.
     통계를 낸다면 평가를 한사람 안한사람, 중도참여 중단한사람의 비율 등을 조사할것이라 생각됨
-
-
-
      */
+
+    @Enumerated(EnumType.STRING)
+    private ParticipationStatus participationStatus;
+    @Enumerated(EnumType.STRING)
+    private DepositStatus depositStatus;
+    @Enumerated(EnumType.STRING)
+    private RatingStatus ratingStatus;
+
+    LocalDateTime depositReturnDate;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    // A list to hold each status without persisting it to the database
+    @Transient
+    private List<Enum<?>> statuses;
+
+    // You can initialize this list in the constructor or a method
+    public void initializeStatuses() {
+        this.statuses = List.of(participationStatus, depositStatus, ratingStatus);
+    }
 
 
 

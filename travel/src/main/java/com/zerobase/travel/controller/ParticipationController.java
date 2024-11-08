1edd60e655c46a96eb481e68d17c50303e40101a
Byeon-Sanghwa <bsh6226@gmail.com>
2024-11-08 11:57:48 +0900

    feat) service layer 변경에 따른 repo, controller 수정

--- travel/src/main/java/com/zerobase/travel/controller/ParticipationController.java
+++ travel/src/main/java/com/zerobase/travel/controller/ParticipationController.java
@@ -1,6 +1,7 @@
 package com.zerobase.travel.controller;
 
 import com.zerobase.travel.dto.ParticipationDto;
+import com.zerobase.travel.service.ParticipationManagementService;
 import com.zerobase.travel.service.ParticipationService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ParticipationController {
 
     private final ParticipationService participationService;
+    private final ParticipationManagementService participationManagementService;
 
     @PostMapping("{postId}/participations")
     public ParticipationDto createParticipation(
@@ -30,27 +32,27 @@ public class ParticipationController {
 
     }
 
-    // 참여취소
+    // 개인의 참여취소
     @GetMapping("{postId}/participations{participationId}")
     public ResponseEntity<Object> deleteParticipations( @PathVariable Long postId,  @RequestHeader(value="userId") String userId ) {
 
-        participationService.unjoinWithDepositPenaltyParticipation(postId,userId);
+        participationManagementService.unjoinWithDepositReturnedParticipation(postId,userId);
 
         return null;
     }
 
 
 
-    // 참여자 조회시에 Status에 Join 상태의 유저 확인
+    // 참여자 조회시에 Status에 Join 상태의 유저 리스트 확인
     @GetMapping("{postId}/participations")
     public ResponseEntity<List<ParticipationDto>> getParticipationsByPost(
         @PathVariable Long postId) {
         log.info("getParticipationsByPost controller start");
         return ResponseEntity.ok(
-            participationService.getParticipationsStatusOfJoin(postId));
+            participationService.getParticipationsDtosStatusOfJoin(postId));
     }
 
-    // 유저들의 완료된 여행에 대해서
+    // 유저들의 완료된 여행에 대해서 숫자 반환
     @GetMapping("/participations/by-userid/{userId}")
     public ResponseEntity<Integer> getParticipationsCompletedByUserId(
         @PathVariable String userId) {

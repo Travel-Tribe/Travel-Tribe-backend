4c6a6be7005a19d869b8a0f495026004ee2c201f
byeonsanghwa <sh.byeon20@gmail.com>
2024-11-09 18:27:31 +0900

    refactor) 반환 dto에 보증금, 평가, 여행상태 추가

--- travel/src/main/java/com/zerobase/travel/dto/ParticipationDto.java
+++ travel/src/main/java/com/zerobase/travel/dto/ParticipationDto.java
@@ -1,6 +1,9 @@
 package com.zerobase.travel.dto;
 
 import com.zerobase.travel.entity.ParticipationEntity;
+import com.zerobase.travel.type.DepositStatus;
+import com.zerobase.travel.type.ParticipationStatus;
+import com.zerobase.travel.type.RatingStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,6 +19,9 @@ public class ParticipationDto {
     private Long participationId;
     private Long postId;
     private String userId;
+    private ParticipationStatus participationStatus;
+    private DepositStatus depositStatus;
+    private RatingStatus ratingStatus;
 
 
     public static ParticipationDto fromEntity(ParticipationEntity participationEntity) {
@@ -23,6 +29,9 @@ public class ParticipationDto {
             .participationId(participationEntity.getParticipationId())
             .postId(participationEntity.getPostEntity().getPostId())
             .userId(participationEntity.getUserId())
+            .participationStatus(participationEntity.getParticipationStatus())
+            .depositStatus(participationEntity.getDepositStatus())
+            .ratingStatus(participationEntity.getRatingStatus())
             .build();
 
     }

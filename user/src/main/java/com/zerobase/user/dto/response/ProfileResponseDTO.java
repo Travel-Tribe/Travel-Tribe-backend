ebc619224523dcf8576effc990bf2933e209fcab
SeokSeungMin <seoksmin@gmail.com>
2024-11-12 17:14:30 +0900

    user Redis final

--- user/src/main/java/com/zerobase/user/dto/response/ProfileResponseDTO.java
+++ user/src/main/java/com/zerobase/user/dto/response/ProfileResponseDTO.java
@@ -1,5 +1,10 @@
 package com.zerobase.user.dto.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.zerobase.user.entity.LangAbilityEntity;
 import com.zerobase.user.entity.ProfileEntity;
 import com.zerobase.user.entity.VisitedCountryEntity;
@@ -9,10 +14,15 @@ import com.zerobase.user.type.Smoking;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
+@Getter
+@Builder
 @AllArgsConstructor
+@JsonDeserialize(builder = ProfileResponseDTO.ProfileResponseDTOBuilder.class)
 public class ProfileResponseDTO {
 
     private Long id;
@@ -21,11 +31,20 @@ public class ProfileResponseDTO {
     private MBTI mbti;
     private Smoking smoking;
     private Gender gender;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate birth;
     private Double ratingAvg;
     private String fileAddress;
     private List<String> langAbilities;
     private List<String> visitedCountries;
+
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class ProfileResponseDTOBuilder {
+
+    }
 
     // 정적 팩토리 메서드 추가
     public static ProfileResponseDTO fromEntity(ProfileEntity profileEntity) {

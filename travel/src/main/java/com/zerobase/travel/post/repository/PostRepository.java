df75879088ed3abd8384e18340ba504f29f6f6c7
shbyeon20 <157558259+shbyeon20@users.noreply.github.com>
2024-11-12 16:55:57 +0900

    Revert "feat) 참여자 조건 검증 로직 추가"

--- travel/src/main/java/com/zerobase/travel/post/repository/PostRepository.java
+++ travel/src/main/java/com/zerobase/travel/post/repository/PostRepository.java
@@ -1,7 +1,6 @@
 package com.zerobase.travel.post.repository;
 
 import com.zerobase.travel.post.entity.PostEntity;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -10,7 +9,4 @@ import org.springframework.stereotype.Repository;
 public interface PostRepository extends JpaRepository<PostEntity, Long>, JpaSpecificationExecutor<PostEntity> {
 
     boolean existsByPostIdAndUserId(long postId, long organizerUserId);
-
-
-    Optional<PostEntity> findByPostId(long postId);
 }

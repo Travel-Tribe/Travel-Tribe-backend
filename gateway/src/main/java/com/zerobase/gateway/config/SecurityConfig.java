05f5dcbf14777d46dedfbb04193507c68312ebcd
kim975 <81149759+kim975@users.noreply.github.com>
2024-11-19 20:16:00 +0900

    fix : 다른 도메인에서 로그아웃이 불가능 하던 문제 수정

--- gateway/src/main/java/com/zerobase/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/zerobase/gateway/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.LogoutSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -15,16 +16,9 @@ public class SecurityConfig {
         http
             .csrf(ServerHttpSecurity.CsrfSpec::disable) // CSRF 보호 비활성화
             .authorizeExchange(exchange -> exchange
-                .pathMatchers("/logout").permitAll() // /logout 경로 허용
                 .anyExchange().permitAll() // 모든 요청 허용
             )
-            .logout(logout -> logout
-                .logoutUrl("/logout") // 로그아웃 경로 설정
-                .logoutSuccessHandler((exchange, authentication) -> {
-                    // 로그아웃 성공 시 처리 로직 (필요할 경우)
-                    return exchange.getExchange().getResponse().setComplete();
-                })
-            );
+            .logout(LogoutSpec::disable);
 
         return http.build();
     }

40df526731e949c5747e4d6234c0a9279c3c8bbf
shbyeon20 <157558259+shbyeon20@users.noreply.github.com>
2024-11-21 12:14:56 +0900

    fix) files가 초기화 되지 않는 경우에 nullpoint Exception handling을 위해 초기화

--- travel/src/main/java/com/zerobase/travel/communities/type/RequestCreateCommunity.java
+++ travel/src/main/java/com/zerobase/travel/communities/type/RequestCreateCommunity.java
@@ -4,9 +4,8 @@ import com.zerobase.travel.typeCommon.Continent;
 import com.zerobase.travel.typeCommon.Country;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,8 +13,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class RequestCreateCommunity {
 
     @NotNull
@@ -28,5 +25,12 @@ public class RequestCreateCommunity {
     String title;
     @NotBlank
     String content;
-    List<String> files;
+    List<String> files = new ArrayList<>();
+
+
+
 }
+
+
+
+

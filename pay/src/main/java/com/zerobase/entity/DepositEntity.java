f0b598c808501df2ec7045ae572fc500c2274fcf
shbyeon20 <157558259+shbyeon20@users.noreply.github.com>
2024-11-23 20:55:54 +0900

    feat) pay모듈 entity jpa auditing기능추가

--- pay/src/main/java/com/zerobase/entity/DepositEntity.java
+++ pay/src/main/java/com/zerobase/entity/DepositEntity.java
@@ -2,17 +2,22 @@ package com.zerobase.entity;
 
 import com.zerobase.model.type.PaymentStatus;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
@@ -20,6 +25,7 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class DepositEntity {
 
     @Id
@@ -30,4 +36,9 @@ public class DepositEntity {
     private String userId;
     @Enumerated(EnumType.STRING)
     private PaymentStatus paymentStatus;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }

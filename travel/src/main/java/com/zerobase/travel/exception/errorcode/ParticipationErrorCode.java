7e24582074aa2b45eda9dc2e38302fe24f7c3283
shbyeon20 <157558259+shbyeon20@users.noreply.github.com>
2024-11-23 19:13:55 +0900

    feat) lock service 수정

--- travel/src/main/java/com/zerobase/travel/exception/errorcode/ParticipationErrorCode.java
+++ travel/src/main/java/com/zerobase/travel/exception/errorcode/ParticipationErrorCode.java
@@ -14,7 +14,9 @@ public enum ParticipationErrorCode implements ErrorCode {
     POST_PARTICIPATION_LIMIT("ERROR-PARTICIPATION-00005", "모집글이 수용할 수 있는 참가인원을 초과하였습니다."),
     APPLICANT_POST_LIMIT_MATCHED("ERROR-PARTICIPATION-00006", "모집글의 제한사항과 유저의 프로필이 일치합니다"),
     PARTICIPATION_STATUS_ERROR("ERROR-PARTICIPATION-00007", "참여자의 참여상태가 예상과 다릅니다"),
-    PARTICIPATION_NOT_EXIST("ERROR-PARTICIPATION-00008", "찾고자하는 참여가 존재하지 않습니다");
+    PARTICIPATION_NOT_EXIST("ERROR-PARTICIPATION-00008", "찾고자하는 참여가 존재하지 않습니다"),
+    PARTICIPATION_CONTENTION("ERROR-PARTICIPATION-00009", "Lock 획득에 실패하였습니다."),
+    LOCK_ACQUISITION_ERROR("ERROR-PARTICIPATION-00010", "Lock 획득 중 예외가 발생하였습니다");
 
 
     private final String errorCode;

8a664cbf502c2df62a125c9030ad09d89b206627
kim975 <81149759+kim975@users.noreply.github.com>
2024-12-17 14:17:13 +0900

    Update ParticipationController.java

--- travel/src/main/java/com/zerobase/travel/controller/v2/ParticipationController.java
+++ travel/src/main/java/com/zerobase/travel/controller/v2/ParticipationController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(value = "api/v2/posts/")
-@RestController
+@RestController("participationControllerV2")
 @RequiredArgsConstructor
 @Slf4j
 public class ParticipationController {
